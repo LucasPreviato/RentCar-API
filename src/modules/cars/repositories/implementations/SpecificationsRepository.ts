43d34cbc6a595c326e3b392c5152d00dd01da5c5
Lucas Previato <lucaspreviato@live.com>
2022-05-18 17:17:34 -0300

    Seperando as implementações das interfaces na pasta repositories para facilitar na organização do codigo

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
-import { Specification } from "../model/Specification";
-import { ICreateSpecificationDTO, ISpecificationsReposity } from "./ISpecificationsReposity";
+import { Specification } from "../../model/Specification";
+import { ICreateSpecificationDTO, ISpecificationsReposity } from "../ISpecificationsReposity";
 
 
 
